1bcd0be7f8168cc14e2b69ce5edf848447382dd0
Anudeep <anudeep.hanuman@gmail.com>
2022-09-19 01:36:41 +0530

    started about us

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -8,8 +8,11 @@ export default function about() {
             <div className="sit-image-container">
                 <img src={logo} alt="sit" />
             </div>
+            <h1 className="about-us-heading">About Us</h1>
             <div className="about-us-text-container">
-                <p className="about-us-text"></p>
+                <p className="about-us-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, nesciunt officiis voluptas molestiae error a suscipit asperiores sit amet! Soluta accusamus fugit, perferendis quo sint veniam consequuntur maiores corrupti odit laborum rerum aliquid quam laudantium temporibus culpa in deleniti rem similique, quidem, excepturi doloribus quisquam! Cum vero officia sed quisquam.
+                </p>
             </div>
         </section>
     )
