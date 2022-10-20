40edf7198922f8d79e8d1c691453b45214adafb9
Gurucharan046 <dkgurucharan@gmail.com>
2022-10-20 11:57:22 +0530

    added the media queries at the breakpoint 702px and 340px in the registrationInfo.css file

--- src/pages/registrationInfo/registrationInfo.jsx
+++ src/pages/registrationInfo/registrationInfo.jsx
@@ -17,37 +17,37 @@ function RegistrationInfo()
         <th className='col-head'>Non IEEE Member</th>
       </tr>
       <tr>
-        <td className='col-head'>Indian Authors</td>
+        <td className='col-head  two-row-col-1 text-head'>Indian Authors</td>
         <td>Students</td>
-        <td>Rs. 5000</td>
-        <td>Rs. 6000</td>
+        <td>Rs. 4500</td>
+        <td>Rs. 6500</td>
       </tr>
       <tr>
-        <td></td>
+      <td className='two-row-col-2'></td>
         <td>Faculty, Industry</td>
-        <td>Rs. 6000</td>
+        <td>Rs. 4800</td>
         <td>Rs. 7000</td>
       </tr>
       <tr>
-        <td className='col-head'>Foreign Authors</td>
+        <td className='col-head two-row-col-1 text-head'>Foreign Authors</td>
         <td>Students</td>
-        <td>70 USD</td>
+        <td>55 USD</td>
         <td>80 USD</td>
       </tr>
       <tr>
-        <td></td>
+        <td className='two-row-col-2'></td>
         <td>Faculty, Industry</td>
-        <td>80 USD</td>
+        <td>75 USD</td>
         <td>90 USD</td>
       </tr>
       <tr>
         <td className='col-head'>Attendees</td>
-        <td>Rs. 1000</td>
+        <td className='last-row'>Rs. 1000</td>
       </tr>
     </table>
 
-    <h1 className="reg-info-heading">
-      <span className="reg-info-part">Registration Fee for </span> Offline <span className="reg-info-part">Participants</span>
+    <h1 className="reg-info-heading offline-head">
+      <span className="reg-info-part offline-head">Registration Fee for </span> Offline <span className="reg-info-part offline-head">Participants</span>
     </h1>
     <table className="reg-table-details">
       <tr>
@@ -57,32 +57,32 @@ function RegistrationInfo()
         <th className='col-head'>Non IEEE Member</th>
       </tr>
       <tr>
-        <td className='col-head'>Indian Authors</td>
+        <td className='col-head two-row-col-1 text-head'>Indian Authors</td>
         <td>Students</td>
-        <td>Rs. 5500</td>
-        <td>Rs. 6500</td>
+        <td>Rs. 4800</td>
+        <td>Rs. 7000</td>
       </tr>
       <tr>
-        <td></td>
+      <td className='two-row-col-2'></td>
         <td>Faculty, Industry</td>
-        <td>Rs. 6500</td>
+        <td>Rs. 5200</td>
         <td>Rs. 7500</td>
       </tr>
       <tr>
-        <td className='col-head'>Foreign Authors</td>
+        <td className='col-head two-row-col-1 text-head'>Foreign Authors</td>
         <td>Students</td>
-        <td>75 USD</td>
+        <td>70 USD</td>
         <td>85 USD</td>
       </tr>
       <tr>
-        <td></td>
+      <td className='two-row-col-2'></td>
         <td>Faculty, Industry</td>
-        <td>85 USD</td>
+        <td>80 USD</td>
         <td>95 USD</td>
       </tr>
       <tr>
         <td className='col-head'>Attendees</td>
-        <td>Rs. 2000</td>
+        <td className='last-row'>Rs. 2000</td>
       </tr>
     </table>
   </div>
