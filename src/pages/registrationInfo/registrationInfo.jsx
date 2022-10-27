effb04216b64f959709f8c201f61399556e1d5e4
Gurucharan046 <dkgurucharan@gmail.com>
2022-10-28 00:19:39 +0530

    edited the table in the registration page now it has only one table the next changes is awaited

--- src/pages/registrationInfo/registrationInfo.jsx
+++ src/pages/registrationInfo/registrationInfo.jsx
@@ -7,9 +7,9 @@ function RegistrationInfo()
   return(
     <div className="detailsShower">
     <h1 className="reg-info-heading">
-      <span className="reg-info-part">Registration Fee for </span> Online <span className="reg-info-part">Participants</span>
+      <span className="reg-info-part">Registration Fee for </span><span className="reg-info-part">Participants</span>
     </h1>
-    <table className="reg-table-details">
+     <table className="reg-table-details">
       <tr>
         <th></th>
         <th></th>
@@ -18,73 +18,36 @@ function RegistrationInfo()
       </tr>
       <tr>
         <td className='col-head  two-row-col-1 text-head'>Indian Authors</td>
-        <td>Students</td>
-        <td>Rs. 4500</td>
-        <td>Rs. 6500</td>
+        <td>Academia</td>
+        <td>Rs. 5000</td>
+        <td>Rs. 7500</td>
       </tr>
       <tr>
       <td className='two-row-col-2'></td>
-        <td>Faculty, Industry</td>
-        <td>Rs. 4800</td>
-        <td>Rs. 7000</td>
+        <td>Industry</td>
+        <td>Rs. 6500</td>
+        <td>Rs. 9500</td>
       </tr>
       <tr>
-        <td className='col-head two-row-col-1 text-head'>Foreign Authors</td>
-        <td>Students</td>
-        <td>55 USD</td>
+        <td className='col-head two-row-col-1 text-head f-aurth'>Foreign Authors</td>
+        <td className='f-aurth-2'></td>
         <td>80 USD</td>
+        <td>120 USD</td>
       </tr>
-      <tr>
+      {/* <tr>
         <td className='two-row-col-2'></td>
         <td>Faculty, Industry</td>
         <td>75 USD</td>
         <td>90 USD</td>
-      </tr>
+      </tr> */}
       <tr>
-        <td className='col-head'>Attendees</td>
-        <td className='last-row'>Rs. 1000</td>
-      </tr>
-    </table>
-
-    <h1 className="reg-info-heading offline-head">
-      <span className="reg-info-part offline-head">Registration Fee for </span> Offline <span className="reg-info-part offline-head">Participants</span>
-    </h1>
-    <table className="reg-table-details">
-      <tr>
-        <th></th>
-        <th></th>
-        <th className='col-head'>IEEE Member</th>
-        <th className='col-head'>Non IEEE Member</th>
-      </tr>
-      <tr>
-        <td className='col-head two-row-col-1 text-head'>Indian Authors</td>
-        <td>Students</td>
-        <td>Rs. 4800</td>
-        <td>Rs. 7000</td>
-      </tr>
-      <tr>
-      <td className='two-row-col-2'></td>
-        <td>Faculty, Industry</td>
-        <td>Rs. 5200</td>
-        <td>Rs. 7500</td>
-      </tr>
-      <tr>
-        <td className='col-head two-row-col-1 text-head'>Foreign Authors</td>
-        <td>Students</td>
-        <td>70 USD</td>
-        <td>85 USD</td>
-      </tr>
-      <tr>
-      <td className='two-row-col-2'></td>
-        <td>Faculty, Industry</td>
-        <td>80 USD</td>
-        <td>95 USD</td>
-      </tr>
-      <tr>
-        <td className='col-head'>Attendees</td>
+        <td className='col-head two-row-col-1 text-head'>Attendees</td>
         <td className='last-row'>Rs. 2000</td>
       </tr>
     </table>
+
+   
+  
   </div>
   )   
 }
